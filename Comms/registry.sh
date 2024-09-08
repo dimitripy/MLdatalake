0dd81e7fa81ef75616be480259b24f90eb661741
dimitripy <waldschmidt92@googlemail.com>
2024-09-08 20:52:09 +0000

    Backup DAG erstellt - ungetestet

--- Comms/registry.sh
+++ Comms/registry.sh
@@ -7,6 +7,8 @@ REGISTRY_FILE="$REGISTRY_DIR/airflow_dag_registry.yaml"
 register_in_registry() {
   PROJECT_NAME=$1
   DAG_PATH=$2
+  BACKUP_DIR=$3
+  API_KEY_FILE=$4
 
   echo "Registriere Unterprojekt in der zentralen DAG-Registry..."
 
@@ -29,7 +31,7 @@ register_in_registry() {
   # Überprüfen, ob das Projekt bereits in der Registry eingetragen ist
   if grep -q "^  $PROJECT_NAME:" "$REGISTRY_FILE"; then
     # Aktuellen Pfad des Projekts finden
-    CURRENT_PATH=$(grep -A 1 "^  $PROJECT_NAME:" "$REGISTRY_FILE" | grep "dag_path:" | awk '{print $2}')
+    CURRENT_PATH=$(grep -A 3 "^  $PROJECT_NAME:" "$REGISTRY_FILE" | grep "dag_path:" | awk '{print $2}')
     
     # Überprüfen, ob der Pfad übereinstimmt
     if [ "$CURRENT_PATH" != "$DAG_PATH" ]; then
@@ -37,6 +39,8 @@ register_in_registry() {
       
       # Aktualisieren des Pfads in der Registry
       sudo sed -i "/^  $PROJECT_NAME:/!b;n;c\    dag_path: $DAG_PATH" "$REGISTRY_FILE"
+      sudo sed -i "/^  $PROJECT_NAME:/!b;n;n;c\    backup_dir: $BACKUP_DIR" "$REGISTRY_FILE"
+      sudo sed -i "/^  $PROJECT_NAME:/!b;n;n;n;c\    api_key_file: $API_KEY_FILE" "$REGISTRY_FILE"
       echo "Der Pfad für das Unterprojekt $PROJECT_NAME wurde erfolgreich aktualisiert."
     else
       echo "Das Unterprojekt $PROJECT_NAME ist bereits mit dem richtigen Pfad in der Registry registriert."
@@ -45,6 +49,8 @@ register_in_registry() {
     # Projekt zur Registry hinzufügen
     echo "  $PROJECT_NAME:" | sudo tee -a "$REGISTRY_FILE" > /dev/null
     echo "    dag_path: $DAG_PATH" | sudo tee -a "$REGISTRY_FILE" > /dev/null
+    echo "    backup_dir: $BACKUP_DIR" | sudo tee -a "$REGISTRY_FILE" > /dev/null
+    echo "    api_key_file: $API_KEY_FILE" | sudo tee -a "$REGISTRY_FILE" > /dev/null
     echo "Unterprojekt $PROJECT_NAME wurde erfolgreich in die Registry eingetragen."
   fi
 }
@@ -72,13 +78,13 @@ trigger_sync_dag() {
 }
 
 # Überprüfen, ob die erforderlichen Argumente übergeben wurden
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <PROJECT_NAME> <DAG_PATH>"
+if [ "$#" -ne 4 ];hen
+  echo "Usage: $0 <PROJECT_NAME> <DAG_PATH> <BACKUP_DIR> <API_KEY_FILE>"
   exit 1
 fi
 
 # Aufruf der Funktion zur Registrierung
-register_in_registry "$1" "$2"
+register_in_registry "$1" "$2" "$3" "$4"
 
 # Trigger den Sync-DAG nach der Registrierung
 trigger_sync_dag
