c2bfb34ef8d30e71b88d969793eea8925fd37681
dimitripy <waldschmidt92@googlemail.com>
2024-09-21 13:42:13 +0000

    aktueller Stand vom workflow

--- control_center.sh
+++ control_center.sh
@@ -8,6 +8,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/Comms/common_functions.sh"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 COMPOSE_DIR="$SCRIPT_DIR/../"  # Verzeichnis mit der docker-compose.yml
 ENV_FILE="$SCRIPT_DIR/.env"  # Pfad zur .env Datei
+CONFIG_FILE="$SCRIPT_DIR/../datalake_dags/config.json"  # Pfad zur config.json Datei
 
 # Definiere den Projektnamen für das Logging
 PROJECT_NAME="mldatalake"
@@ -20,9 +21,24 @@ else
     exit 1
 fi
 
+# Funktion zum Erstellen der config.json Datei
+create_config_json() {
+    cat <<EOF > "$CONFIG_FILE"
+{
+    "db_user": "$MYSQL_USER",
+    "db_password": "$MYSQL_PASSWORD",
+    "db_host": "localhost",
+    "db_name": "$MYSQL_DATABASE",
+    "colab_url": "$COLAB_URL"
+}
+EOF
+    echo "config.json Datei wurde erfolgreich erstellt unter $CONFIG_FILE"
+}
+
 # Menü für Benutzerinteraktion
 echo "Bitte wähle eine Option:"
 echo "1 - Datenbank erstellen und initialisieren"
+echo "2 - config.json Datei erstellen"
 echo "3 - Starten oder Neustarten des Docker-Containers"
 echo "5 - Stoppen des Docker-Containers"
 echo "8 - Lösche den Container und die Daten des Projekts"
@@ -35,6 +51,10 @@ case $choice in
         log "$PROJECT_NAME" "Starte Datenbankerstellung und Initialisierung..."
         # Führe das create_database.sh-Skript aus
         bash "$SCRIPT_DIR/scripts/create_database.sh"
+        ;;
+    2)
+        log "$PROJECT_NAME" "Erstelle config.json Datei..."
+        create_config_json
         ;;
     3)
         log "$PROJECT_NAME" "Starten oder Neustarten des Docker-Containers..."
@@ -59,22 +79,17 @@ case $choice in
         fi
         ;;
     9)
-        echo -e "\e[41m\e[97mWARNUNG: Dies wird ALLE Dateien und Verzeichnisse löschen und neu erstellen!\e[0m"
-        read -p "Möchtest du wirklich fortfahren? (yes/no): " confirmation
-        if [ "$confirmation" == "yes" ]; then
-            log "$PROJECT_NAME" "Führe Hard Reset durch..."
-            cd "$COMPOSE_DIR"
-            docker-compose down -v
-            bash "$SCRIPT_DIR/scripts/create_database.sh"
-        else
-            log "$PROJECT_NAME" "Hard Reset vom Benutzer abgebrochen."
-        fi
+        log "$PROJECT_NAME" "Hard Reset wird durchgeführt..."
+        cd "$COMPOSE_DIR"
+        docker-compose down -v
+        bash "$SCRIPT_DIR/scripts/create_database.sh"
+        docker-compose up -d
         ;;
     0)
-        log "$PROJECT_NAME" "Beenden ohne Änderungen."
+        echo "Beenden..."
         exit 0
         ;;
     *)
-        echo "Ungültige Auswahl. Bitte 1, 3, 5, 8, 9 oder 0 wählen."
+        echo "Ungültige Eingabe. Bitte wähle eine gültige Option."
         ;;
 esac
